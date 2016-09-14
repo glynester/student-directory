045023b50cc425130e93e2eb00551c5f7ec5bc22
Glenn <glynester@hotmail.com>
2016-09-14 09:54:09 +0000

    Added var for student_count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+#First we print the list of students.
 puts "The Students of Villains Academy"
 puts "________________________________"
 puts "Dr. Hannibal Lecter"
@@ -14,8 +16,6 @@ puts "Norman Bates"
 puts "The big green blob"
 puts "Jonny Rotten"
 puts "Dick Dastardly"
-
-print "Overall, we have "
-print 11
-puts " great students"
+#Finally we print the total number of students.
+print "Overall, we have #{student_count} great students"
 
